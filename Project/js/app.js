16d88163ee539f01646cf5898dcbe8c025a83971
Shahla Mustafayeva <sehla066@gmail.com>
2021-03-25 11:01:28 +0400

    app.js elave edildi

---
+++ Project/js/app.js
@@ -0,0 +1,15 @@
+const nav = document.querySelector('#navigation');
+const navTop = nav.offsetTop;
+
+function stickyNavigation() {
+  console.log('navTop = ' + navTop);
+  console.log('scrollY = ' + window.scrollY);
+
+  if (window.scrollY >= navTop) {
+    document.body.classList.add('fixed-nav');
+  } else {
+    document.body.classList.remove('fixed-nav');
+  }
+}
+
+window.addEventListener('scroll', stickyNavigation); 
