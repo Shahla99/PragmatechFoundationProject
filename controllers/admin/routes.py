f66bfb434c031b1ce7a0daf949b6ccb4e8fffe11
Shahla Mustafayeva <sehla066@gmail.com>
2021-05-07 00:27:11 +0400

    db

--- controllers/admin/routes.py
+++ controllers/admin/routes.py
@@ -1,4 +1,4 @@
-from app import app
+from run import app
 from flask import render_template,request,redirect
 
 @app.route('/admin')
--- run.py
+++ run.py
@@ -1,4 +1,14 @@
-from app import app
+from flask import Flask 
+from flask_sqlalchemy import SQLAlchemy
+
+app=Flask(__name__)
+
+
+#admin routes
+
+from controllers.admin.routes import *
+
+from controllers.main.routes import * 
 
 if __name__=='__main__':
     app.run(debug=True)
