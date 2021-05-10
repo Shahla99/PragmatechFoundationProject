3d06e487a59e740203b652f49bc9073c9d417027
Shahla Mustafayeva <sehla066@gmail.com>
2021-05-10 19:11:40 +0400

    slider table yaradildi

--- controllers/main/routes.py
+++
@@ -1,6 +0,0 @@
-from run import app
-from flask import render_template,request,redirect
-
-@app.route('/')
-def main_index():
-    return render_template('main/index.html')
--- models.py
+++
@@ -1,7 +0,0 @@
-from app import db
-class Slider(db.Model):
-    id=db.Column(db.Integer,primary_key=True)
-    s_image=db.Column(db.String(50))
-    s_title=db.Column(db.String(50))
-    s_text=db.Column(db.String(50))
-    s_url=db.Column(db.String(50))
